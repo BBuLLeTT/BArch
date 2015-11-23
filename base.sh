cdd0a8202ba1a36267af24fceaeea9e2eb5a3f18
BBuLLeTT <solo996@gmail.com>
2015-11-23 19:46:07 +0400

    Fixes

--- base.sh
+++ base.sh
@@ -4,8 +4,13 @@ if [ $# -eq 0 ]
     echo "No arguments supplied"
     exit
 fi
+curl -O https://raw.githubusercontent.com/BBuLLeTT/BArch/master/barch.service
+curl -O https://raw.githubusercontent.com/BBuLLeTT/BArch/master/barch.sh
+curl -O https://raw.githubusercontent.com/BBuLLeTT/BArch/master/chroot.sh
 #pacstrap -i $1 base base-devel --noconfirm
 #genfstab -U $1 > $1/etc/fstab
-cp ./chrootscript $1/
-arch-chroot $1 /bin/bash /chrootscript
+cp ./chroot.sh $1/
+cp ./barch.service $1/etc/systemd/system/
+cp ./barch.sh $1/etc/profile.d/
+arch-chroot $1 /bin/bash /chroot.sh
 #reboot
