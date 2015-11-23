26d5f0b9314badef05f3754ed268c90d06e5aa3b
BBuLLeTT <solo996@gmail.com>
2015-11-23 21:57:50 +0400

    Fixes

--- barch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-localectl set-x11-keymap us,ge
-localectl set-locale LC_NUMERIC=ka_GE.UTF-8 LC_TIME=ka_GE.UTF-8 LC_MONETARY=ka_GE.UTF-8 LC_PAPER=ka_GE.UTF-8 LC_MEASUREMENT=ka_GE.UTF-8
-systemctl disable barch
--- base.sh
+++ base.sh
@@ -4,11 +4,9 @@ if [ $# -eq 0 ]
     echo "No arguments supplied"
     exit
 fi
-curl -O https://raw.githubusercontent.com/BBuLLeTT/BArch/master/barch.sh
 curl -O https://raw.githubusercontent.com/BBuLLeTT/BArch/master/chroot.sh
 pacstrap -i $1 base base-devel --needed --noconfirm 
 genfstab -U $1 > $1/etc/fstab
 cp ./chroot.sh $1/
-cp ./barch.sh $1/
 arch-chroot $1 /bin/bash /chroot.sh
 #reboot
